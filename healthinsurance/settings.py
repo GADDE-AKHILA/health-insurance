9caa455ec0e1b97abd52d2f0d43a32424890f232
GADDE-AKHILA <akhilagadde2023@gmail.com>
2024-07-23 00:10:03 +0530

    updated s3 access url

--- healthinsurance/settings.py
+++ healthinsurance/settings.py
@@ -75,6 +75,20 @@ TEMPLATES = [
 WSGI_APPLICATION = 'healthinsurance.wsgi.application'
 CSRF_COOKIE_SECURE=False
 
+# # AWS S3 Configuration
+AWS_STORAGE_BUCKET_NAME= os.environ['AWS_STORAGE_BUCKET_NAME']
+AWS_S3_REGION_NAME = 'us-east-1'
+AWS_DEFAULT_REGION='us-east-1'
+AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
+AWS_ACCESS_KEY_ID = os.environ['AWS_ACCESS_KEY_ID']
+AWS_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
+AWS_LOCATION = 'media'
+AWS_S3_FILE_OVERWRITE = False
+AWS_DEFAULT_ACL = None
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{AWS_LOCATION}/'
+# AWS_STORAGE_BUCKET_NAME = os.environ['AWS_STORAGE_BUCKET_NAME']
+
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
